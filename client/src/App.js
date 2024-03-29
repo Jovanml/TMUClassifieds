aebbb4f8cd725dd81e2b365fcb9477566e3c6f10
mlec01 <ylimechiu@gmail.com>
2024-03-29 05:42:27 -0400

    add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header.js';
 
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* TODO 
+          - fix home path to point to homepage
+          - add paths for other pages
+          - route the buttons in header
+        */}
+        <Route path='/' element={<Header />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
