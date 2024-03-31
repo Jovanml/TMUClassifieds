b257258ccb9b56a69904de1e6c28a96557388f72
Theodore-Wilson <theowilson2014@gmail.com>
2024-03-31 15:46:27 -0400

    switched from props to url search params

--- client/src/components/Message/MessageController.js
+++ client/src/components/Message/MessageController.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './Style/MessageComponent.css'
 
@@ -6,48 +6,52 @@ import MessagePage from './MessagePage'
 import MessageBar from './MessageBar'
 
 import { socket } from './socket'
+import { useLocation } from 'react-router-dom'
 
-function MessageController({user, messageUser, messageUsername}){
+function MessageController({ user }) {
 
-    
+
 
     socket.disconnect()
     socket.connect()
 
-    //const usersConvo = useRef(user['conversations'])
+    const { search } = useLocation();
+    const query = new URLSearchParams(search)
+
+    const messageUser = query.get("messageUser") === null ? '' : query.get("messageUser")
+    const messageUsername = query.get("messageUsername") === null ? '' : query.get("messageUsername")
+
     const [usersConvo, setUsersConvo] = useState(user['conversations'])
 
-    const [messageUserID, setMessageUserID] = useState(messageUser === '' ? Object.keys(usersConvo).length > 0 ? usersConvo[Object.keys(usersConvo)[0]] : {'id': '', 'name': ''} : {'id': messageUser, 'name': messageUsername})
+    const [messageUserID, setMessageUserID] = useState(messageUser === '' ? Object.keys(usersConvo).length > 0 ? { 'id': Object.keys(usersConvo)[0], 'name': usersConvo[Object.keys(usersConvo)[0]]['name'] } : { 'id': '', 'name': '' } : { 'id': messageUser, 'name': messageUsername })
     const mID = messageUserID['id']
     const mN = messageUserID['name']
 
     useEffect(() => {
-        async function fetchConvos(){
+        async function fetchConvos() {
             fetch('http://127.0.0.1:5000/get/user?id=' + user['_id'] + '&field=conversations', {
-                method:'GET'
+                method: 'GET'
             }).then(response => {
-                if (response.status === 200){
-                  response.json().then((data) => {
-                    setUsersConvo(data['conversations'])
-                  })
+                if (response.status === 200) {
+                    response.json().then((data) => {
+                        setUsersConvo(data['conversations'])
+                    })
                 }
-              })
+            })
         }
         fetchConvos()
     }, [messageUserID])
 
-    if (!Object.keys(usersConvo).includes(mID)){
-
-        //usersConvo[mID] = {'id': user['_id'] + mID, 'name': mN}
+    if (!Object.keys(usersConvo).includes(mID)) {
         setUsersConvo({
             ...usersConvo,
-            [mID]: {'id': user['_id'] + mID, 'name': mN}
+            [mID]: { 'id': user['_id'] + mID, 'name': mN }
         })
         fetch('http://127.0.0.1:5000/add/user/conversation', {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
-              },
+            },
             body: JSON.stringify({
                 'id': user['_id'],
                 'name': user['name'],
@@ -58,14 +62,14 @@ function MessageController({user, messageUser, messageUsername}){
         })
     }
 
-    if (messageUserID === ''){
-      return (
-      <div className='MessageComponent'>
-        <MessageBar setMessageUserID={setMessageUserID} usersConvos={usersConvo} selectedConvo={messageUserID}/>
-      </div>
-      )
+    if (messageUserID === '') {
+        return (
+            <div className='MessageComponent'>
+                <MessageBar setMessageUserID={setMessageUserID} usersConvos={usersConvo} selectedConvo={messageUserID} />
+            </div>
+        )
     } else {
-        return(
+        return (
             <div className='MessageComponent'>
                 <MessagePage user={user} usersConvo={usersConvo} messageUserID={mID} setMessageUserID={setMessageUserID} statePass={messageUserID} />
             </div>
