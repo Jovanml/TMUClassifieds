628453d1e189a54a187d72ca35e8f1b86378ad75
mlec01 <ylimechiu@gmail.com>
2024-04-01 19:20:21 -0400

    create heading for modal body content

---
+++ client/src/components/Heading.js
@@ -0,0 +1,22 @@
+const Heading = ({
+  title, // string
+  subtitle, // string
+  subheading, // string
+  center // boolean
+}) => {
+  return (
+    <div className={center ? 'heading-text-center' : 'heading-text-start'}>
+      <div className='heading-title'>
+        {title}
+      </div>
+      <div className='heading-subheading'>
+        {subheading}
+      </div>
+      <div className='heading-subtitle'>
+        {subtitle}
+      </div>
+    </div>
+  );
+}
+
+export default Heading;
