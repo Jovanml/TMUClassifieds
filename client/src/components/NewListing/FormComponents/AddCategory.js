e035ad9c0a88156f7a5f05af50ec227f71f9e6fc
Jovanml <jovan.ml2001@gmail.com>
2024-04-02 22:34:10 -0400

    fixed styling

--- client/src/components/NewListing/FormComponents/AddCategory.js
+++ client/src/components/NewListing/FormComponents/AddCategory.js
@@ -23,12 +23,12 @@ const AddCategory = () => {
             <Box gap='30px' display='flex' mt='40px'>
                 {categories.map((category, i) => {
                     return (
-                        <Box key={i} width='150px' border='1px solid #F1F3F6' borderRadius='20px' 
+                        <Box key={i} width='200px' border='1px solid #F1F3F6' borderRadius='20px' 
                         boxShadow='0px 2px 5px 0px rgba(0, 0, 0, 0.25);' p='30px 30px 20px 30px'
                         display='flex' flexDirection='column' alignItems='center'
                         sx={{"&:hover": {cursor: 'pointer', backgroundColor: '#E7EAF4'}}}
                         >   
-                            <Box width='50%' mb='20px'>
+                            <Box width='50%' display='flex' flexDirection='column' alignItems='center' mb='20px'>
                                 {category.icon}
                             </Box>
                             {category.label}
