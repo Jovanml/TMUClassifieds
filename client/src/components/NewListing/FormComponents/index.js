a367aaff9e4d492964eacfdf2e3e113381ed8401
Theodore-Wilson <theowilson2014@gmail.com>
2024-04-04 01:54:16 -0400

    added location to newpost

--- client/src/components/NewListing/FormComponents/index.js
+++ client/src/components/NewListing/FormComponents/index.js
@@ -3,3 +3,4 @@ export { default as AddDescription } from './AddDescription.js';
 export { default as AddPhoto } from './AddPhoto.js';
 export { default as AddPrice } from './AddPrice.js';
 export { default as AddTitle } from './AddTitle.js';
+export { default as AddLoc } from './AddLoc.js'
