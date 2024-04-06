4b99b41ef93ec4537647812abd4671f9ba77d8b9
Theodore-Wilson <theowilson2014@gmail.com>
2024-04-05 22:01:32 -0400

    index comments

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,19 @@
+//Hooks
 import React from 'react';
+
+//DOM
 import ReactDOM from 'react-dom/client';
+
+//Stlyes
 import './index.css';
+
+//Compnents
 import App from './App';
+
+//Global Context
 import { GlobalContextProvider } from './contexts/GlobalContext';
 
+//Render webpage
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<GlobalContextProvider>
